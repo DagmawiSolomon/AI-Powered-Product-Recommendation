365a801c23f845c602c1f36fd8208e8264119241
Dagmawi Solomon <02dagmawisolomon@gmail.com>
2025-09-25 16:32:17 +0300

    fix: correct the value being returned by getEmbeddings

--- ai-powered-product-recommendation/src/app/api/ai/embeddings/route.ts
+++ ai-powered-product-recommendation/src/app/api/ai/embeddings/route.ts
@@ -2,45 +2,37 @@ import { NextResponse } from "next/server";
 import { getToken } from "@/lib/auth-server";
 import { GoogleGenAI } from "@google/genai";
 
+async function getEmbeddings(contents: string | string[]) {
+  const ai = new GoogleGenAI({ apiKey: process.env.GOOGLE_GEMINI_API_KEY });
 
-async function getEmbeddings(contents: string | string[]){
-    const ai = new GoogleGenAI({apiKey: process.env.GOOGLE_GEMINI_API_KEY});
+  const response = await ai.models.embedContent({
+    model: "gemini-embedding-001",
+    contents,
+    config: {
+      taskType: "SEMANTIC_SIMILARITY",
+      outputDimensionality: 768,
+    },
+  });
 
-    const response = await ai.models.embedContent({
-        model: 'gemini-embedding-001',
-        contents,
-        config:{
-            taskType: 'SEMANTIC_SIMILARITY',
-            outputDimensionality: 768,
-        }
-        
-    });
-
-    return response.embeddings?.values
-
+  return response.embeddings; // array of { values }
 }
 
+export async function POST(request: Request) {
+  const token = await getToken();
+  if (!token) {
+    return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
+  }
 
-// args
-// text: number[]
+  const body = await request.json();
 
-export async function POST(request: Request){
-    const token = await getToken();  
-    if (!token) {
-        return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
-    }
+  if (!body.text) {
+    return NextResponse.json({ error: "Missing Text Field" }, { status: 400 });
+  }
 
-    const body = await request.json();
+  const embeddings = await getEmbeddings(body.text);
 
-    if(!body.text){
-        return NextResponse.json({ error: "Missing Text Field" }, { status: 400 });
-    }
-    
-    const text  = await getEmbeddings(body.text);
-
- 
-        return NextResponse.json({
-            success: true,
-            text,
-        });
-    }
+  return NextResponse.json({
+    success: true,
+    embeddings, // consistent with Gemini docs
+  });
+}

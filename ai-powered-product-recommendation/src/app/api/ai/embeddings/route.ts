d4e7c50a27f78b2b71d7fdbd192bb91799d28a4e
Dagmawi Solomon <02dagmawisolomon@gmail.com>
2025-09-25 13:24:38 +0300

    feat: create an endpoint to call gemini  for embedding

---
+++ ai-powered-product-recommendation/src/app/api/ai/embeddings/route.ts
@@ -0,0 +1,46 @@
+import { NextResponse } from "next/server";
+import { getToken } from "@/lib/auth-server";
+import { GoogleGenAI } from "@google/genai";
+
+
+async function getEmbeddings(contents: string | string[]){
+    const ai = new GoogleGenAI({apiKey: process.env.GOOGLE_GEMINI_API_KEY});
+
+    const response = await ai.models.embedContent({
+        model: 'gemini-embedding-001',
+        contents,
+        config:{
+            taskType: 'SEMANTIC_SIMILARITY',
+            outputDimensionality: 768,
+        }
+        
+    });
+
+    return response.embeddings?.values
+
+}
+
+
+// args
+// text: number[]
+
+export async function POST(request: Request){
+    const token = await getToken();  
+    if (!token) {
+        return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
+    }
+
+    const body = await request.json();
+
+    if(!body.text){
+        return NextResponse.json({ error: "Missing Text Field" }, { status: 400 });
+    }
+    
+    const text  = await getEmbeddings(body.text);
+
+ 
+        return NextResponse.json({
+            success: true,
+            text,
+        });
+    }

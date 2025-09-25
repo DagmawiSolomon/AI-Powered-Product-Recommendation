d79fb7cec68c9cc1525332378871de50e92b1f5b
Dagmawi Solomon <02dagmawisolomon@gmail.com>
2025-09-25 20:49:16 +0300

    fix: remove unneccessary imports

--- ai-powered-product-recommendation/convex/search_history/mutations.ts
+++ ai-powered-product-recommendation/convex/search_history/mutations.ts
@@ -2,6 +2,8 @@ import { mutation } from "../_generated/server";
 import { searchHistoryFields } from "./searchHistoryFields";
 import { authComponent } from "../auth";
 import { api } from "../_generated/api";
+import { internal } from "../_generated/api";
+import { AnyApi } from "convex/server";
 
 
 
@@ -27,6 +29,8 @@ const search_history_id:any = await ctx.db.insert("search_history", {
   error_message: args.error_message,
   user: user._id, 
 });
-    return search_history_id;
+
+return search_history_id;
   },
 });
+

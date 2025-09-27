7339850e8565567d11954b0128acb79219fae161
Dagmawi Solomon <02dagmawisolomon@gmail.com>
2025-09-27 22:35:12 +0300

    feat: add mutation to trigger workflow and add fields to create product mutation to match the schema

--- ai-powered-product-recommendation/convex/products/mutations.ts
+++ ai-powered-product-recommendation/convex/products/mutations.ts
@@ -1,6 +1,9 @@
 import { mutation } from "../_generated/server";
 import { productFields } from "./productFields";
 import { v } from "convex/values";
+import { internal } from "../_generated/api";
+import { Id } from "../_generated/dataModel";
+
 
 
 export const CreateProducts= mutation({
@@ -16,6 +19,8 @@ export const CreateProducts= mutation({
           price: p.price,
           image: p.image,
           url: p.url,
+          tags:p.tags,
+          search_text: p.search_text,
           description: p.description,
           embedding: p.embedding,
         });
@@ -27,5 +32,28 @@ export const CreateProducts= mutation({
     }
 
     return results;
+  },
+});
+
+
+export const startHybirdSearchWorkflow = mutation({
+  args: { searchId: v.string() },
+  handler: async (ctx, args) => {
+    // Fetch the search_history document
+    const searchHistory = await ctx.db.get(args.searchId as Id<"search_history">);
+    if (!searchHistory) {
+      throw new Error("search_history not found");
+    }
+
+    // Update the status
+    await ctx.db.patch(args.searchId as Id<"search_history">, { status: "processing" });
+
+    // Schedule the action with user_query from the document
+    const result = await ctx.scheduler.runAfter(0, internal.products.actions.HybridSearchWorkFlow, {
+      search_id: args.searchId as Id<"search_history">,
+      user_query: searchHistory.prompt,
+    });
+
+    console.log(result);
   },
 });

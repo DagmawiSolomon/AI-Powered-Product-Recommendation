50a97c7e50483a007829d51c0787d54c34478ff7
Dagmawi Solomon <02dagmawisolomon@gmail.com>
2025-09-28 16:42:31 +0300

    feat: add new fields

--- ai-powered-product-recommendation/convex/products/productFields.ts
+++ ai-powered-product-recommendation/convex/products/productFields.ts
@@ -2,10 +2,12 @@ import {v} from "convex/values"
 
 export const productFields = {
     name: v.string(),
-    category: v.array(v.string()),
+    category: v.string(),
     price: v.number(),
     image: v.string(),
     url: v.string(),
     description: v.string(),
-    embedding: v.optional(v.array(v.float64()))
+    tags: v.array(v.string()),
+    embedding: v.optional(v.array(v.float64())),
+    search_text: v.string(),
 }

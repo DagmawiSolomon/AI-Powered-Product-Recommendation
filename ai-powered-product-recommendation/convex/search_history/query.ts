f32d54149ff7552156567fcb0fc9ce4f4157f45a
Dagmawi Solomon <02dagmawisolomon@gmail.com>
2025-09-28 14:15:38 +0300

    feat: implement query to fetch status and results

--- ai-powered-product-recommendation/convex/search_history/query.ts
+++ ai-powered-product-recommendation/convex/search_history/query.ts
@@ -1,6 +1,6 @@
 import { query } from "../_generated/server";
 import { v } from "convex/values";
-
+import { Id } from "../_generated/dataModel";
 // This query expects a userId (string) and returns the matching user from your "users" table
 export const getUserByUserId = query({
   args: { userId: v.string() },
@@ -13,3 +13,21 @@ export const getUserByUserId = query({
     return user;
   },
 });
+
+export const checkStatus = query({
+  args: { id: v.string()},
+  handler: async (ctx, args) => {
+    const history_id = args.id as Id<"search_history">
+    const doc = await ctx.db.get(history_id);
+    return doc?.status;
+  },
+})
+
+export const getResult = query({
+  args: { id: v.string()},
+  handler: async (ctx, args) => {
+    const history_id = args.id as Id<"search_history">
+    const doc = await ctx.db.get(history_id);
+    return doc?.result;
+  },
+})

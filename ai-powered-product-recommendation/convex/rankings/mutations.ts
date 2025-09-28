debb8f918dc43794e73f20aa9e606e199975076b
Dagmawi Solomon <02dagmawisolomon@gmail.com>
2025-09-28 16:49:06 +0300

    feat: implement mutation to insert rankings and trigger comparison creation

--- ai-powered-product-recommendation/convex/rankings/mutations.ts
+++ ai-powered-product-recommendation/convex/rankings/mutations.ts
@@ -1,13 +1,37 @@
 import { mutation } from "../_generated/server";
 import { rankingsFields } from "./schema";
 import { v } from "convex/values";
+import { internal } from "../_generated/api";
+
+export type aiSelections = {
+    id: string,
+    reasoning: string,
+}[]
 
 export const createRankings = mutation({
   args: { rankings: v.array(v.object(rankingsFields)) },
   handler: async (ctx, args) => {
+     const aiSelections: aiSelections = [];
+     const search_id = args.rankings[0].searchId;
+
      for (const ranking of args.rankings){
-         await ctx.db.insert("rankings", ranking)
+      const id = await ctx.db.insert("rankings", ranking)
+      if (ranking.aiRank && [1, 2, 3].includes(ranking.aiRank)) {
+        aiSelections.push({
+          id,
+          reasoning: ranking.reason || "",
+        });
+      }
      }
-    
+     
+   if (aiSelections) {
+      await ctx.scheduler.runAfter(0, internal.comparisons.actions.generateComparison, {
+        search_id,
+        aiSelections
+      });
+    }
   },
 });
+
+
+

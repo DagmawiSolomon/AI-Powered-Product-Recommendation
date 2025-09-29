78f6467278cd49df791681d8bc623043e82438bb
Dagmawi Solomon <02dagmawisolomon@gmail.com>
2025-09-29 19:21:14 +0300

    feat: add new loader

--- ai-powered-product-recommendation/src/app/search/[id]/loading.tsx
+++ ai-powered-product-recommendation/src/app/search/[id]/loading.tsx
@@ -1,15 +1,5 @@
 export default function Loading() {
   return (
-    <div className="banter-loader">
-      <div className="banter-loader__box"></div>
-      <div className="banter-loader__box"></div>
-      <div className="banter-loader__box"></div>
-      <div className="banter-loader__box"></div>
-      <div className="banter-loader__box"></div>
-      <div className="banter-loader__box"></div>
-      <div className="banter-loader__box"></div>
-      <div className="banter-loader__box"></div>
-      <div className="banter-loader__box"></div>
-    </div>
+    <span className="loader"></span>
   )
 }
